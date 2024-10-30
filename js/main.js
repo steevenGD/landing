78546ee4f2c48a5fbf6c3cfa7c1de880a6547f0f
unknown <sjgomez@espol.edu.ec>
2024-10-30 14:32:24 -0500

    Avance

--- js/main.js
+++ js/main.js
@@ -23,7 +23,9 @@ let sendData = ( ) => {
   })
   .then(result => {
       alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
-      form.reset()
+      form.reset();
+      // Recuperación de datos
+      getData();
   })
   .catch(error => {
       alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
@@ -31,9 +33,71 @@ let sendData = ( ) => {
 
 }
 
+let getData = async () => {
+  try {
+
+      // Realiza la petición fetch a la URL de la base de datos
+      const response = await fetch(databaseURL, {
+        method: 'GET'
+      });
+
+      // Verifica si la respuesta es exitosa
+      if (!response.ok) {
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+      }
+
+      // Convierte la respuesta en formato JSON
+      let data = await response.json();
+
+      if(data != null) {
+
+        // Cuente el número de suscriptores registrados por fecha a partir del objeto data
+        let countSuscribers = new Map()
+
+        if (Object.keys(data).length > 0) {
+            for (let key in data) {
+   
+                let { email, saved } = data[key]
+                   
+                let date = saved.split(",")[0]
+                   
+                let count = countSuscribers.get(date) || 0;
+                countSuscribers.set(date, count + 1)
+            }
+        }
+      
+        // Genere y agregue filas de una tabla HTML para mostrar fechas y cantidades de suscriptores almacenadas 
+        if (countSuscribers.size > 0) {
+
+          subscribers.innerHTML = ''
+ 
+          let index = 1;
+          for (let [date, count] of countSuscribers) {
+            let rowTemplate = `
+            <tr>
+                <th>${index}</th>
+                <td>${date}</td>
+                <td>${count}</td>
+            </tr>`
+              subscribers.innerHTML += rowTemplate
+              index++;
+          }
+      }
+    }
+
+    } catch (error) {
+      // Muestra cualquier error que ocurra durante la petición
+      alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+    }
+} 
+
+
+
 
 let ready = () => {
   console.log('DOM está listo')
+  // Recuperación de datos
+  getData();
   //debugger
 }
 
